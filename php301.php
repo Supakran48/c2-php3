9d8718acf7d9d49740ace41a6c8ff86db8d13c7c
Supakran48 <flukeark1493@gmail.com>
2021-11-19 10:39:06 +0700

    update

--- php301.php
+++ php301.php
@@ -1,3 +1,7 @@
 <?php
-echo "welcome to My site"
+    $x = 10;
+    $y = 5;
+    echo "<h1>x = $x and y = $y"," <hr color ='red' siz='4' /></h1>";
+    echo "x - y = ",$x - $y,"<br />";
+    echo "x * y = ",$x - $y,"<br />";
 ?>
--- php302.php
+++ php302.php
@@ -1,4 +1,13 @@
 <?php
-   echo "operator";
+$x = 120;
 
+echo "<h1>PHP Arithmetic Operators</h1>";
+echo "<h1>x = $x <br /></h1>";
+echo "<hr color='red' size='4px' />";
+
+echo "x += : ", $x += 10,"<br />";//$x = $x + 10
+echo "x -= : ", $x -= 10,"<br />";//$x = $x - 10
+echo "x *= : ", $x *= 10,"<br />";//$x = $x * 10
+echo "x /= : ",$x /= 10,"<br />";//$x = $x / 10
+echo "x %= : ",$x %= 10,"<br />";//$x = $x % 10
 ?>
